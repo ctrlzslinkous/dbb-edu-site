2dcf87e8bc256c7ec2ef66e62beb0bf0a9b41662
Zaz Linkous <zaz@controlzee.com>
2023-07-12 12:56:28 -0700

    Convert element number to typescript

--- lib/numberElements.ts
+++ lib/numberElements.ts
@@ -1,17 +1,23 @@
 import { visit } from "unist-util-visit"
+import hast from 'hast'
+import {Root} from 'hast'
+import {Element} from 'hast'
 
 
-export default function numberElements(options){
+export default function numberElements(options: { tagNames: never[]; numberPunctuation: string; prefixNumbers: boolean }){
     const tagNames = options.tagNames || []
     const numberPunctuation = options.numberPunctuation || " "
     const prefixNumbers = options.prefixNumbers || false
     const counters = new Array(tagNames.length).fill(1)
-    return (tree, file)=>{
-        visit(tree,'element', function (node) {
+    return (tree:Root, file: any)=>{
+        visit(tree,'element', function (node: Element) {
                 for(let i = 0; i < tagNames.length; i++){
                     if(node.tagName == tagNames[i]){
-                        if(!node.properties.className) node.properties.className = []
-                        node.properties.className.push(["numbered", tagNames[i], counters[i]].join('-'))
+                        if(!node.properties) continue
+                        if(!node.properties.className) node.properties.className = [] as string[]
+                        if(Array.isArray(node.properties.className)){
+                            node.properties.className.push(["numbered", tagNames[i], counters[i]].join('-'))
+                        }
                         if(prefixNumbers){
                             const firstChild = node.children[0]
                             if(firstChild.type == 'text'){
